21f4d54b58147f38322d7e4fcc938479b993c752
Aayush Sinha <aayush.aryan@me.com>
2017-04-02 01:46:09 +0530

    First Commit

---
+++ include/header.php
@@ -0,0 +1,122 @@
+<header id="header" class="clearfix" data-ma-theme="blue">
+    <ul class="h-inner">
+        <li class="hi-trigger ma-trigger" data-ma-action="sidebar-open" data-ma-target="#sidebar">
+            <div class="line-wrap">
+                <div class="line top"></div>
+                <div class="line center"></div>
+                <div class="line bottom"></div>
+            </div>
+        </li>
+
+        <li class="hi-logo hidden-xs">
+            <a href="index.html">Silencio Admin</a>
+        </li>
+
+        <li class="pull-right">
+            <ul class="hi-menu">
+
+                <li data-ma-action="search-open">
+                    <a href=""><i class="him-icon zmdi zmdi-search"></i></a>
+                </li>
+
+                <li class="dropdown">
+                    <a data-toggle="dropdown" href="">
+                        <i class="him-icon zmdi zmdi-notifications"></i>
+                        <i class="him-counts">9</i>
+                    </a>
+                    <div class="dropdown-menu dropdown-menu-lg pull-right">
+                        <div class="list-group him-notification">
+                            <div class="lg-header">
+                                Notification
+
+                                <ul class="actions">
+                                    <li class="dropdown">
+                                        <a href="" data-ma-action="clear-notification">
+                                            <i class="zmdi zmdi-check-all"></i>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="lg-body">
+                                <a class="list-group-item media" href="">
+                                    <div class="pull-left">
+                                        <img class="lgi-img" src="img/demo/profile-pics/1.jpg" alt="">
+                                    </div>
+                                    <div class="media-body">
+                                        <div class="lgi-heading">David Belle</div>
+                                        <small class="lgi-text">Cum sociis natoque penatibus et magnis dis parturient montes</small>
+                                    </div>
+                                </a>
+                                <a class="list-group-item media" href="">
+                                    <div class="pull-left">
+                                        <img class="lgi-img" src="img/demo/profile-pics/2.jpg" alt="">
+                                    </div>
+                                    <div class="media-body">
+                                        <div class="lgi-heading">Jonathan Morris</div>
+                                        <small class="lgi-text">Nunc quis diam diamurabitur at dolor elementum, dictum turpis vel</small>
+                                    </div>
+                                </a>
+                                <a class="list-group-item media" href="">
+                                    <div class="pull-left">
+                                        <img class="lgi-img" src="img/demo/profile-pics/3.jpg" alt="">
+                                    </div>
+                                    <div class="media-body">
+                                        <div class="lgi-heading">Fredric Mitchell Jr.</div>
+                                        <small class="lgi-text">Phasellus a ante et est ornare accumsan at vel magnauis blandit turpis at augue ultricies</small>
+                                    </div>
+                                </a>
+                                <a class="list-group-item media" href="">
+                                    <div class="pull-left">
+                                        <img class="lgi-img" src="img/demo/profile-pics/4.jpg" alt="">
+                                    </div>
+                                    <div class="media-body">
+                                        <div class="lgi-heading">Glenn Jecobs</div>
+                                        <small class="lgi-text">Ut vitae lacus sem ellentesque maximus, nunc sit amet varius dignissim, dui est consectetur neque</small>
+                                    </div>
+                                </a>
+                                <a class="list-group-item media" href="">
+                                    <div class="pull-left">
+                                        <img class="lgi-img" src="img/demo/profile-pics/4.jpg" alt="">
+                                    </div>
+                                    <div class="media-body">
+                                        <div class="lgi-heading">Bill Phillips</div>
+                                        <small class="lgi-text">Proin laoreet commodo eros id faucibus. Donec ligula quam, imperdiet vel ante placerat</small>
+                                    </div>
+                                </a>
+                            </div>
+
+                            <a class="view-more" href="">View Previous</a>
+                        </div>
+                    </div>
+                </li>
+                <li class="dropdown">
+                    <a data-toggle="dropdown" href=""><i class="him-icon zmdi zmdi-more-vert"></i></a>
+                    <ul class="dropdown-menu dm-icon pull-right">
+                        
+                        <li class="hidden-xs">
+                            <a data-ma-action="fullscreen" href=""><i class="zmdi zmdi-fullscreen"></i> Toggle Fullscreen</a>
+                        </li>
+                        <li>
+                            <a data-ma-action="clear-localstorage" href=""><i class="zmdi zmdi-delete"></i> Clear Local Storage</a>
+                        </li>
+                        <li>
+                            <a href=""><i class="zmdi zmdi-face"></i> Privacy Settings</a>
+                        </li>
+                        <li>
+                            <a href=""><i class="zmdi zmdi-settings"></i> Other Settings</a>
+                        </li>
+                    </ul>
+                </li>
+               
+            </ul>
+        </li>
+    </ul>
+
+    <!-- Top Search Content -->
+    <div class="h-search-wrap">
+        <div class="hsw-inner">
+            <i class="hsw-close zmdi zmdi-arrow-left" data-ma-action="search-close"></i>
+            <input type="text">
+        </div>
+    </div>
+</header>
