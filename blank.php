69e8ad8583b05b79fc2147afe191184e212ddb94
Aayush Sinha <aayush.aryan@me.com>
2017-04-02 02:02:00 +0530

    Blank Module Added

---
+++ blank.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<!--[if IE 9 ]><html class="ie9"><![endif]-->
+<html>
+    <?php require_once ("include/head.php"); ?>
+    <body>
+        <?php require_once ("include/header.php"); ?>
+
+        <section id="main">
+            <?php require_once("include/sidebar.php"); ?>
+        </section>
+        
+        
+        
+        <section id="content">
+        
+        
+        
+        </section>
+        
+        
+        
+        
+
+        <?php require_once("include/footer.php"); ?>
+
+        <?php require_once("include/preloader.php"); ?>
+
+       <?php require_once("include/old_ie.php"); ?>
+
+        <?php require_once("include/js.php"); ?>
+
+        <!-- Placeholder for IE9 -->
+        <!--[if IE 9 ]>
+            <script src="vendors/bower_components/jquery-placeholder/jquery.placeholder.min.js"></script>
+        <![endif]-->
+
+        <script src="js/app.min.js"></script>
+    </body>
+  </html>
